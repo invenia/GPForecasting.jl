024b87e47bcc6cd453c8b73ec53a3010488459ca
Eric Perim <eric.perim@invenialabs.co.uk>
2018-11-27 12:34:13 +0000

    Remove Pkg.clone from build

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,2 @@
 # Install the correct branch of HelloBatch
-try
-    Pkg.installed("HelloBatch")
-catch
-    Pkg.clone("git@gitlab.invenia.ca:invenia/HelloBatch.jl.git")
-    Pkg.checkout("HelloBatch", "ls/getrevinfo") # Checkout the branch with working function saving for GPs
-    Pkg.build("HelloBatch")
-end
+Pkg.checkout("HelloBatch", "ls/getrevinfo") # Checkout the branch with working function saving for GPs
