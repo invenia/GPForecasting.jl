6588e033b0dbf004c3062a42c4809957e0d79c82
Eric Davies <iamed2@gmail.com>
2019-01-21 08:57:41 -0600

    Remove mydumbsum

--- src/core/pairwise.jl
+++ src/core/pairwise.jl
@@ -9,17 +9,6 @@ end
 @unionise pairwise_dist(x::AbstractArray, y::Number) = pairwise_dist(x, [y])
 @unionise pairwise_dist(x::Number, y::Number) = pairwise_dist([x], [y])
 
-# This function is very inneficient and is only used as a temporary work-around a Nabla
-# issue. Once that is solved, we should move back to using sumdims(x.^2, 2)
-@unionise function mydumbsum(x::AbstractArray)
-    xs = [x[:, i] for i in 1:size(x, 2)]
-    out = zeros(size(x, 1))
-    for i in 1:size(x, 2)
-       out += xs[i]
-    end
-    return out
-end
-
 @unionise function sq_pairwise_dist(x::AbstractArray, y::AbstractArray)
     return sumdims(x.^2, 2) .+ sumdims(y.^2, 2)' .- 2x * y'
 end
