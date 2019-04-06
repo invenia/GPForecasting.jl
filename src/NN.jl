27731aaf34bc16a899007149ed85792e470b0895
Eric Perim <eric.perim@invenialabs.co.uk>
2019-04-06 15:49:40 +0100

    Add BatchNorm and activation functions

--- src/NN.jl
+++ src/NN.jl
@@ -19,3 +19,21 @@ mutable struct NNLayer <: AbstractNode
     σ::Fixed{Function} # activation function
 end
 (layer::NNLayer)(x) = layer.σ.(layer.W * x + layer.B)
+
+mutable struct BatchNormLayer <: AbstractNode
+    γ # if you want to use different values for each dimension, use a RowVector
+    β # if you want to use different values for each dimension, use a RowVector
+end
+function (layer::BatchNormLayer)(x)
+    means = mean(x, dims=1)
+    stds = std(x, dims=1)
+    x_norm = (x .- means) ./ (stds .+ 1e-10) # Adding 1e-10 for numerical stability
+    return unwrap(layer.γ) .* x_norm .+ unwrap(layer.β)
+end
+
+# Just a few activation functions that may be handy
+relu(x) = max(0, x)
+noisy_relu(x; σ=0.01) = max(0, x + σ * randn())
+leaky_relu(x; α=0.01) = x > 0.0 ? x : α * x
+softplus(x) = log(1 + exp(x))
+sigmoid(x) = 1/(1 + exp(-x))
