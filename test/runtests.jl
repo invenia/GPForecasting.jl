96e5f64f84627bfba66334fce4b33b9cddcd4f36
Eric Perim <eric.perim@invenialabs.co.uk>
2019-05-10 18:18:12 +0100

    Break down test sets

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,24 +16,20 @@ using Test
 _ATOL_ = 1e-5
 
 
-# Write your own tests here.
-@testset "GPForecasting.jl" begin
-
-    # Keep Logging to a minimum
-    logger = Memento.config!("warn"; fmt="[{level} | {name}]: {msg}")
-
-    include("core/util.jl")
-    include("core/optimise.jl")
-    include("pairwise.jl")
-    include("core/algebra.jl")
-    include("core/node.jl")
-    include("core/parameter.jl")
-    include("kernel.jl")
-    include("mean.jl")
-    include("gp.jl")
-    include("gaussian.jl")
-    include("pdf.jl")
-    include("core/nabla.jl")
-    include("synthetic.jl")
-    include("NN.jl")
-end
+# Keep Logging to a minimum
+logger = Memento.config!("warn"; fmt="[{level} | {name}]: {msg}")
+#
+include("core/util.jl")
+include("core/optimise.jl")
+include("pairwise.jl")
+include("core/algebra.jl")
+include("core/node.jl")
+include("core/parameter.jl")
+include("kernel.jl")
+include("mean.jl")
+include("gp.jl")
+include("gaussian.jl")
+include("pdf.jl")
+include("core/nabla.jl")
+include("synthetic.jl")
+include("NN.jl")
