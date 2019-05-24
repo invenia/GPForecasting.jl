aa036a5344906adc0b9d80ac75d745737eed3c46
Eric Perim <eric.perim@invenialabs.co.uk>
2019-05-24 15:19:46 +0100

    Fix breaking test

--- test/core/optimise.jl
+++ test/core/optimise.jl
@@ -25,20 +25,21 @@
         ngp = learn(gp, x, y, objective, trace=false)
         @test 1.5 < ngp.k.stretch.p < 3.5
 
-        # test the OLMM learn method
-        xs = hcat([sin.(2π*collect(0:0.1:2)./i) for i in 1:3]...)
-        A = ones(5,5) + 2Eye(5)
-        U, S, V = svd(A)
-        H = U * Diagonal(S)[:, 1:3]
-        y = (H * xs')'
-        gp = GP(OLMMKernel(3, 5, 10., 1e-2, H, [periodicise(EQ(), i) for i in 1:3]))
-        x = collect(0:0.1:2)
-        ngp = learn(gp, x, y, objective, opt_U=true, its=3, trace=false)
-        Ug = GPForecasting.unwrap(gp.k.U)
-        @test sum(Ug' * Ug) ≈ 3.0 atol = _ATOL_
-        ngp = learn(gp, x, y, objective, its=3, K_U_cycles=2, trace=false)
-        Ug = GPForecasting.unwrap(gp.k.U)
-        @test sum(Ug' * Ug) ≈ 3.0 atol = _ATOL_
+    # test the OLMM learn method
+    xs = hcat([sin.(2π*collect(0:0.1:2)./i) for i in 1:3]...)
+    A = ones(5,5) + 2Eye(5)
+    U, S, V = svd(A)
+    H = U * Diagonal(S)[:, 1:3]
+    y = (H * xs')'
+    gp = GP(OLMMKernel(3, 5, 10., 1e-2, H, [periodicise(EQ(), i) for i in 1:3]))
+    gp.k.H = Fixed(gp.k.H)
+    x = collect(0:0.1:2)
+    ngp = learn(gp, x, y, objective, opt_U=true, its=3, trace=false)
+    Ug = GPForecasting.unwrap(gp.k.U)
+    @test sum(Ug' * Ug) ≈ 3.0 atol = _ATOL_
+    ngp = learn(gp, x, y, objective, its=3, K_U_cycles=2, trace=false)
+    Ug = GPForecasting.unwrap(gp.k.U)
+    @test sum(Ug' * Ug) ≈ 3.0 atol = _ATOL_
 
         # Test that the summary is outputted when called
         out = learn_summary(gp, x, y, objective, its=3, trace=false)
